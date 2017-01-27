8223571a5dd1a04ecd8e0cf52f8ec7a54ff4aa82
Egor Ilchenko <zzodoo@gmail.com>
2017-01-27 20:50:59 +0700

    ttt

--- NumberRecognition/NumberRecognition/Network/Manager.cs
+++ NumberRecognition/NumberRecognition/Network/Manager.cs
@@ -28,17 +28,36 @@
             return await Task.Run(() => _network.Compute(img));
         }
 
+        private double[] GetInputVector(int digit)
+        {
+            var vector = new double[10];
+            for (int i = 0; i < 10; i++)
+            {
+                if(i == digit)
+                {
+                    vector[i] = 1;
+                }
+                else
+                {
+                    vector[i] = 0;
+                }
+            }
+            return vector;
+        }
+
         private List<DataSet> GetDataSets()
         {
-            var tuples =
-                Enumerable.Range(0, 10)
-                    .AsParallel()
-                    .Select(x => new { x, bytes = _imgOperations.GetImagesBytesForDigit(x).ToArray() })
-                    .SelectMany(x => x.bytes, (x, b) => new {x.x, b})
-                    .Select(x => new DataSet(x.b, Enumerable.Range(0, 10).Select(ee => (double)(ee == x.x ? 1 : 0)).ToArray()))
-                    .OrderBy(x => Network.GetRandom())
-                    .ToList();
-            return tuples;
-        }
+            var datasets = new List<DataSet>();
+            for (int digit = 0; digit < 10; digit++)
+            {
+                var outputs = _imgOperations.GetImagesBytesForDigit(digit).ToArray();
+                var input = GetInputVector(digit);
+                foreach (var output in outputs)
+                {
+                    datasets.Add(new DataSet(output, input));
+                }
+            }
+            return datasets.OrderBy(x => Network.GetRandom()).ToList();
+        }            
     }
 }
